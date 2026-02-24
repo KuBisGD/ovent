0a42f9ec22bfd88a4a8c16410479d7b4013b1cb7
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-02-24 17:28:54 +0100

    Add new method observeMutual

--- src/Trait/EventTrait.php
+++ src/Trait/EventTrait.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace Ewn\Ovent\Trait;
 
+use Ewn\Ovent\Interface\EventInterface;
+
 trait EventTrait
 {
     use EventEmitterTrait, EventObserverTrait;
+
+    /**
+     * Makes two objects observe each other.
+     *
+     * @param EventInterface $observer
+     * @return void
+     */
+    public function observeMutual(EventInterface $emitterObserver): void
+    {
+        $this->attachObserver($emitterObserver);
+        $emitterObserver->attachObserver($this);
+    }
 }
