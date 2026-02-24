fcabf0f05fbf45e306da0758bcc2c40d56cc2077
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-02-24 17:29:58 +0100

    Add new method observeMutual

--- src/Interface/EventInterface.php
+++ src/Interface/EventInterface.php
@@ -7,4 +7,12 @@ namespace Ewn\Ovent\Interface;
 /**
  * An interface that allows an object to observe and emit events.
  */
-interface EventInterface extends EventEmitterInterface, EventObserverInterface {}
+interface EventInterface extends EventEmitterInterface, EventObserverInterface {
+    /**
+     * Makes two objects observe each other.
+     *
+     * @param EventInterface $observer
+     * @return void
+     */
+    public function observeMutual(EventInterface $observer): void;
+}
