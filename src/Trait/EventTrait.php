2953dae95ce84bd36d654625582d2539e6fd1a26
KuBisGD <142990773+KuBisGD@users.noreply.github.com>
2026-06-18 17:12:50 +0200

    Add method for observing self + lint

--- src/Trait/EventTrait.php
+++ src/Trait/EventTrait.php
@@ -13,12 +13,22 @@ trait EventTrait
     /**
      * Makes two objects observe each other.
      *
-     * @param EventInterface $observer
+     * @param EventInterface $emitterObserver
      * @return void
      */
     public function observeMutual(EventInterface $emitterObserver): void
     {
         $this->attachObserver($emitterObserver);
         $emitterObserver->attachObserver($this);
+    }
+
+    /**
+     * Makes the object observe itself.
+     *
+     * @return void
+     */
+    protected function observeSelf(): void
+    {
+        $this->attachObserver($this);
     }
 }
